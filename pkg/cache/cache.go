49e0c12c990be0f6dd810dec3b8994a8c152e506
DingDean <dingkework@hotmail.com>
2017-12-23 00:03:08 +0800

    [MODIFY] if no cache at all, return zero Aqi struct

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -11,7 +11,8 @@ import (
 func Get(cacheFilepath string) pm25.Aqi {
 	dat, err := ioutil.ReadFile(cacheFilepath)
 	if err != nil {
-		panic(fmt.Sprintf("无法获取缓存, %s", err.Error()))
+		var zeros pm25.Aqi
+		return zeros
 	}
 	var jsdat pm25.Aqi
 	if err = json.Unmarshal(dat, &jsdat); err != nil {
@@ -26,11 +27,11 @@ func IsExpired(then int64) bool {
 	return diff > 3600 // 缓存只有一个小时的时效
 }
 
-func Save(cache pm25.Aqi, cacheFilepath string) error {
+func Save(cache pm25.Aqi, cacheFilepath string) {
+	cache.Time = time.Now().Unix()
 	data, err := json.Marshal(cache)
 	if err != nil {
 		data = []byte(fmt.Sprintf("{Time: %d, Error: %s}", time.Now().Unix(), err.Error()))
 	}
 	ioutil.WriteFile(cacheFilepath, data, 0644)
-	return err
 }
