c9a8e243dec4aa31c6d010c5b63fa271403584ce
DingDean <dingkework@hotmail.com>
2018-04-16 21:26:51 +0800

    refactor: tidy things up a little bit

--- aliyun.go
+++ aliyun.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -22,6 +23,23 @@ type AliRes struct {
 type Aliyun struct {
 	Req     http.Client
 	Appcode string
+}
+
+func makeErrorAqi(err error) Aqi {
+	return Aqi{Error: err.Error()}
+}
+
+func parseBody(res *http.Response) ([]byte, error) {
+	var body []byte
+	if res.StatusCode != 200 {
+		return body, errors.New(res.Status)
+	}
+	// 读取Body
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return body, err
+	}
+	return body, nil
 }
 
 func (x Aliyun) Query(city string) Aqi {
